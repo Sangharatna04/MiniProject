9ec52ef092943626a07a9959bc889237c8e87239
Sangharatna04 <sangharatnatembhurne79@gmail.com>
2026-02-23 23:56:16 +0530

    my first project

---
+++ miniproject.py
@@ -0,0 +1,15 @@
+import random
+target=random.randint(0,100)
+print(target)
+while True:
+    userchoise=int(input("choose a number:"))
+    if(target==userchoise):
+        print("congratulation you win")
+        break
+    elif(userchoise<target):
+        print("you choose very small numbe--->>  choose greter numger of ",userchoise)
+
+    else:
+        print("you choose very large number choose lower number of ",userchoise)
+
+print("----------WELCOME GAME IS OVER-----------")
